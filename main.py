df9ffd7bc192a08d8aeeb824bfca13367d7d5e9c
Anonymous <136169348+LPopivnyak@users.noreply.github.com>
2024-05-26 18:30:17 +0300

    сука

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import json
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+player = {
+    "score": 0,
+    "power": 1
+}
+def read_data():
+    global player
+    try:
+        with open("play.json", "r", encoding="utf-8") as file:
+            player = json.load(file)
+    except:
+        print("невдача((((((")
+
+def save_data():
+    global player
+    try:
+        with open("play.json", "w", encoding="utf-8") as file:
+            json.dump(player, file, indent=4, ensure_ascii=True)
+    except:
+        print("невдача((((((")
 
 class MainScreen(Screen):
     def __init__(self, **kw):
@@ -7,12 +29,35 @@ class MainScreen(Screen):
 
     def click(self):
         self.ids.ball.size_hint = (0.5, 0.5)
+        read_data()
+        player["score"] += player["power"]
+        self.ids.score_lbl.text = "рахунок: " + str(player["score"])
+        save_data()
 
+    def switch_to_shop(self):
+        self.manager.current = "shop"
+
+class MenuScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+class ShopScreen(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def buy(self, price, power):
+        read_data()
+        if price <= player["score"]:
+            player["score"] -= price
+            player["power"] += power
+            save_data()
 
 class ClickerApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name = "main"))
+        sm.add_widget(MenuScreen(name="menu"))
+        sm.add_widget(ShopScreen(name="shop"))
         return sm
 
 app = ClickerApp()
